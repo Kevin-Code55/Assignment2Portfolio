ec269d5921bdde8e6ad33e5eb1c2d9f7ba1f302b
HR1789 <haider.raza2@ontariotechu.net>
2025-11-03 15:16:46 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET home page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Me' });
+});
+
+/* GET home page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
+
+/* GET home page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+
+module.exports = router;
+
